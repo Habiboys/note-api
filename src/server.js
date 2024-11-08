f7ff28da07e10381e64bd2b16b3ff2d803805691
Muhammad Nouval Habibie <nouvalhabibie18@gmail.com>
2024-11-08 14:55:16 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,23 +1,20 @@
-const Hapi = require('@hapi/hapi');
-const routes = require ('./route');
+const Hapi = require("@hapi/hapi");
+const routes = require("./route");
 
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
 
-const init = async ()=>{
-    const server = Hapi.server({
-        port:5000,
-        host:'localhost',
-        routes: {
-            cors: {
-              origin: ['*'],
-            },
-          },
-
-    });
-
-
-    server.route(routes);
-    await server.start();
-    console.log(`Server is running on ${server.info.uri}`)
+  server.route(routes);
+  await server.start();
+  console.log(`Server is running on ${server.info.uri}`);
 };
 
 init();
